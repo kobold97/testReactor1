361df9a5df629358a37b2251106a0ab5e146f975
Szymon Zgola <kobold97@gmail.com>
2019-09-16 17:00:40 +0200

    test weigth checks

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc5/WashingMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc5/WashingMachineTest.java
@@ -1,15 +1,66 @@
 package edu.iis.mto.testreactor.exc5;
 
-import static org.junit.Assert.assertThat;
 
 import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.*;
 
 public class WashingMachineTest {
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
-    }
+	@Mock
+	DirtDetector dirtDetecor;
+	
+	@Mock
+	Engine engine;
+	
+	@Mock
+	WaterPump waterPump;
+	
+	WashingMachine washingMachine;
+	
+    public static final double MAX_WEIGTH_KG = 8;
+    private static final double HALF_MAX_WEIGTH = MAX_WEIGTH_KG / 2;
+		
+	@Before
+	public void setUp() {
+		washingMachine = new WashingMachine(dirtDetecor, engine, waterPump);
+	}
+	
+	@Test 
+	public void tooHeavyWoolenLaundryResultsWithFailure() {
+
+		
+		LaundryBatch.Builder laundryBuilder = LaundryBatch.builder();
+		laundryBuilder.withWeightKg(HALF_MAX_WEIGTH + 1);
+		laundryBuilder.withMaterialType(Material.WOOL);
+		LaundryBatch laundryBatch = laundryBuilder.build();
+		ProgramConfiguration.Builder configurationBuilder = ProgramConfiguration.builder();
+		ProgramConfiguration programConfiguration = configurationBuilder.build();
+		LaundryStatus status = washingMachine.start(laundryBatch, programConfiguration);
+		
+		Assert.assertThat(status.getResult(), Matchers.equalTo(Result.FAILURE));
+		Assert.assertThat(status.getErrorCode(), Matchers.equalTo(ErrorCode.TOO_HEAVY));
+
+	}
+
+	@Test 
+	public void tooHeavyLaundryOtherThanWoolenAndJeansResultsWithFailure() {
+
+		
+		LaundryBatch.Builder laundryBuilder = LaundryBatch.builder();
+		laundryBuilder.withWeightKg(MAX_WEIGTH_KG + 1);
+		laundryBuilder.withMaterialType(Material.COTTON);
+		LaundryBatch laundryBatch = laundryBuilder.build();
+		ProgramConfiguration.Builder configurationBuilder = ProgramConfiguration.builder();
+		ProgramConfiguration programConfiguration = configurationBuilder.build();
+		LaundryStatus status = washingMachine.start(laundryBatch, programConfiguration);
+		
+		Assert.assertThat(status.getResult(), Matchers.equalTo(Result.FAILURE));
+		Assert.assertThat(status.getErrorCode(), Matchers.equalTo(ErrorCode.TOO_HEAVY));
+
+	}
+	
 
 }
